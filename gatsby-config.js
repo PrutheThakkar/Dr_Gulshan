d763b4498c8c1ed820cd9f509079630217ae982b
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-03-27 16:18:53 +0530

    fix: exclude avif files and add wp source timeout config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,3 @@
-// /**
-//  * @type {import('gatsby').GatsbyConfig}
-//  */
-
-// require("dotenv").config()
-// const { createProxyMiddleware } = require("http-proxy-middleware")
-
-// module.exports = {
-//   siteMetadata: {
-//     title: `Gulshan Rohra`,
-//     description: `Gatsby + WordPress (WPGraphQL) site`,
-//     author: `@gulshan-rohra`,
-//     siteUrl: process.env.GATSBY_WEBSITE_URL || "https://drgulshanrohra.com",
-//   },
-//   plugins: [
-//     `gatsby-plugin-image`,
-//     `gatsby-plugin-sharp`,
-//     `gatsby-transformer-sharp`,
-//     `gatsby-plugin-sass`,
-
-//     {
-//       resolve: `gatsby-source-wordpress`,
-//       options: {
-//         url: process.env.GATSBY_WPGRAPHQL_URL || "https://drgulshanrohra.com/graphql",
-//       },
-//     },
-//   ],
-
-//   // Proxy configuration to avoid CORS during development
-//   developMiddleware: app => {
-//     app.use(
-//       "/wp-json",
-//       createProxyMiddleware({
-//         target: "https://drgulshanrohra.com",
-//         changeOrigin: true,
-//         secure: false,
-//       })
-//     )
-//   },
-// }
-
-
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -64,6 +22,23 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         url: process.env.GATSBY_WPGRAPHQL_URL || "https://app.drgulshanrohra.com/graphql",
+        schema: {
+          timeout: 60000,
+          perPage: 20,
+          requestConcurrency: 5,
+        },
+        type: {
+          MediaItem: {
+            localFile: {
+              requestConcurrency: 5,
+              maxFileSizeBytes: 52428800, // 50MB limit
+              excludeByMimeTypes: ["image/avif", "video/mp4", "video/webm"],
+            },
+          },
+        },
+        develop: {
+          hardCacheMediaFiles: true,
+        },
       },
     },
   ],
